f82570d9103f129086d20f2c27c1cafbbd09f276
Rafael Santos <rsantos@pulseinc.com>
2016-10-25 16:58:49 -0400

    AuthenticationController will throw exception if user is not Authenticated

--- src/main/java/com/matchandtrade/rest/v1/controller/AuthenticationController.java
+++ src/main/java/com/matchandtrade/rest/v1/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matchandtrade.authentication.UserAuthentication;
+import com.matchandtrade.authorization.AuthorizationException;
 import com.matchandtrade.rest.Controller;
 import com.matchandtrade.rest.transformer.UserAuthenticationTransformer;
 import com.matchandtrade.rest.v1.json.UserAuthenticationJson;
@@ -17,6 +18,10 @@ public class AuthenticationController extends Controller {
 	public UserAuthenticationJson get() {
 		// Get user authentication
 		UserAuthentication userAuthentication = getUserAuthentication();
+		// Throw AuthorizationException if there is no UserAuthentication. User is not authenticated.
+		if (userAuthentication == null) {
+			throw new AuthorizationException(AuthorizationException.Type.UNAUTHORIZED);
+		}
 		// Transform the response
 		UserAuthenticationJson result = UserAuthenticationTransformer.transform(userAuthentication);
 		return result;
