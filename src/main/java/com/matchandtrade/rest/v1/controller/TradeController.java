314e61dd6b912e79683c2f4d6a0ad9579659b813
Rafael Santos <rafael.santos.bra@gmail.com>
2017-03-30 00:00:36 -0400

    Implemented TradeController.get(params)

--- src/main/java/com/matchandtrade/rest/v1/controller/TradeController.java
+++ src/main/java/com/matchandtrade/rest/v1/controller/TradeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.matchandtrade.authorization.Authorization;
 import com.matchandtrade.common.Pagination;
 import com.matchandtrade.common.SearchCriteria;
+import com.matchandtrade.common.SearchResult;
 import com.matchandtrade.model.TradeModel;
 import com.matchandtrade.persistence.entity.TradeEntity;
 import com.matchandtrade.rest.Controller;
@@ -17,7 +18,7 @@ import com.matchandtrade.rest.v1.transformer.TradeTransformer;
 import com.matchandtrade.rest.v1.validator.TradeValidator;
 
 @RestController
-@RequestMapping(path="/rest/v1/trades/")
+@RequestMapping(path="/rest/v1/trades")
 public class TradeController extends Controller {
 
 	@Autowired
@@ -29,7 +30,7 @@ public class TradeController extends Controller {
 	@Autowired
 	TradeTransformer tradeTransformer;
 
-	@RequestMapping(path="", method=RequestMethod.POST)
+	@RequestMapping(path="/", method=RequestMethod.POST)
 	public TradeJson post(@RequestBody TradeJson requestJson) {
 		// Validate request identity
 		authorization.validateIdentity(getAuthentication());
@@ -44,13 +45,25 @@ public class TradeController extends Controller {
 		return result;
 	}
 
-	@RequestMapping(path="{tradeId}", method=RequestMethod.GET)
+	@RequestMapping(path={"", "/"}, method=RequestMethod.GET)
+	public SearchResult<TradeJson> get(String name, Integer _pageNumber, Integer _pageSize) {
+		// Validate request identity
+		authorization.validateIdentity(getAuthentication());
+		SearchCriteria searchCriteria = new SearchCriteria(new Pagination(_pageNumber, _pageSize));
+		if (name != null) {
+			searchCriteria.addCriterion(TradeEntity.Field.name, name);
+		}
+		// Delegate to model layer
+		SearchResult<TradeEntity> searchResult = tradeModel.search(searchCriteria);
+		// Transform the response
+		SearchResult<TradeJson> result = TradeTransformer.transform(searchResult);
+		return result;
+	}
+	
+	@RequestMapping(path="/{tradeId}", method=RequestMethod.GET)
 	public TradeJson get(@PathVariable("tradeId") Integer tradeId) {
 		// Validate request identity
 		authorization.validateIdentity(getAuthentication());
-		// Build SearchCriteria
-		SearchCriteria searchCriteria = new SearchCriteria(new Pagination(1,1));
-		searchCriteria.addCriterion(TradeEntity.Field.tradeId, tradeId);
 		// Delegate to model layer
 		TradeEntity searchResult = tradeModel.get(tradeId);
 		// Transform the response
