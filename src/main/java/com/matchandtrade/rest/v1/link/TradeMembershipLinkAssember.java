179dd9edcbb8d37aae007a721ff780bbdbd14713
Rafael Santos <rafael.santos.bra@gmail.com>
2018-01-26 23:17:31 -0500

    Added HATEOAS for 'trade' and 'user' in TradeMembershipJson

--- src/main/java/com/matchandtrade/rest/v1/link/TradeMembershipLinkAssember.java
+++ src/main/java/com/matchandtrade/rest/v1/link/TradeMembershipLinkAssember.java
@@ -4,7 +4,9 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import com.matchandtrade.persistence.common.SearchResult;
+import com.matchandtrade.rest.v1.controller.TradeController;
 import com.matchandtrade.rest.v1.controller.TradeMembershipController;
+import com.matchandtrade.rest.v1.controller.UserController;
 import com.matchandtrade.rest.v1.json.TradeMembershipJson;
 
 public class TradeMembershipLinkAssember {
@@ -15,6 +17,8 @@ public class TradeMembershipLinkAssember {
 	public static void assemble(TradeMembershipJson json) {
 		if (json != null) {
 			json.getLinks().add(linkTo(methodOn(TradeMembershipController.class).get(json.getTradeMembershipId())).withSelfRel());
+			json.getLinks().add(linkTo(methodOn(TradeController.class).get(json.getTradeId())).withRel("trade"));
+			json.getLinks().add(linkTo(methodOn(UserController.class).get(json.getUserId())).withRel("user"));
 		}
 	}
 
