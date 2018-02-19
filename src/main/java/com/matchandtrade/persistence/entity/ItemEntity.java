83e836d57bedf82f20c06d0d57b0c15f4f157e98
Rafael Santos <rafael.santos.bra@gmail.com>
2018-02-18 23:48:56 -0500

    Removed WantItemEntity and related files

--- src/main/java/com/matchandtrade/persistence/entity/ItemEntity.java
+++ src/main/java/com/matchandtrade/persistence/entity/ItemEntity.java
@@ -1,18 +1,10 @@
 package com.matchandtrade.persistence.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,7 +13,6 @@ public class ItemEntity implements com.matchandtrade.persistence.entity.Entity {
 	
 	private Integer itemId;
 	private String name;
-	private Set<WantItemEntity> wantItems = new HashSet<>();
 
 	@Id
 	@Column(name = "item_id")
@@ -35,22 +26,12 @@ public class ItemEntity implements com.matchandtrade.persistence.entity.Entity {
 		return name;
 	}
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "item_to_want_item", joinColumns = @JoinColumn(name = "item_id", foreignKey = @ForeignKey(name = "item_to_want_item_item_id_fk")), inverseJoinColumns = @JoinColumn(name = "want_item_id", foreignKey = @ForeignKey(name = "item_to_want_item_want_item_id_fk")))
-	public Set<WantItemEntity> getWantItems() {
-		return wantItems;
-	}
-
 	public void setItemId(Integer itemId) {
 		this.itemId = itemId;
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public void setWantItems(Set<WantItemEntity> wantItems) {
-		this.wantItems = wantItems;
 	}
 
 }
