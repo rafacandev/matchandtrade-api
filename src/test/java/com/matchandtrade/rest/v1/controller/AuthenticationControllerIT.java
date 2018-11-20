7e592421a87f2d52f58cec385fc08e728b614fae
Rafael Santos <rafael.santos.bra@gmail.com>
2018-11-19 23:25:01 -0500

    Migrated AuthenticationControllerIT to MockMvc

---
+++ src/test/java/com/matchandtrade/rest/v1/controller/AuthenticationControllerIT.java
@@ -0,0 +1,76 @@
+package com.matchandtrade.rest.v1.controller;
+
+import com.matchandtrade.persistence.entity.TradeEntity;
+import com.matchandtrade.persistence.entity.UserEntity;
+import com.matchandtrade.persistence.facade.TradeRepositoryFacade;
+import com.matchandtrade.rest.v1.json.TradeJson;
+import com.matchandtrade.rest.v1.transformer.TradeTransformer;
+import com.matchandtrade.test.helper.ControllerHelper;
+import com.matchandtrade.test.random.TradeRandom;
+import com.matchandtrade.test.random.UserRandom;
+import com.matchandtrade.util.JsonUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static com.matchandtrade.persistence.entity.TradeEntity.State.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "file:config/matchandtrade.properties")
+@SpringBootTest
+@WebAppConfiguration
+public class AuthenticationControllerIT {
+
+	private String authorizationHeader;
+	@Autowired
+	private ControllerHelper controllerHelper;
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
+	@Autowired
+	private TradeRandom tradeRandom;
+	@Autowired
+	private TradeRepositoryFacade tradeRepositoryFacade;
+	private TradeTransformer tradeTransformer = new TradeTransformer();
+	private UserEntity user;
+	@Autowired
+	private UserRandom userRandom;
+
+
+	@Before
+	public void before() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		// Reusing user and authorization header for better performance
+		if (user == null) {
+			user = userRandom.createPersistedEntity();
+			authorizationHeader = controllerHelper.generateAuthorizationHeader(user);
+		}
+	}
+
+	@Test
+	public void get_When_AuthenticationExist_Then_Succeeds() throws Exception {
+		mockMvc.perform(
+				get("/matchandtrade-api/v1/authentications/")
+				.header(HttpHeaders.AUTHORIZATION, authorizationHeader)
+			)
+			.andExpect(status().isOk());
+	}
+
+}
--- src/test/java/com/matchandtrade/rest/v1/controller/ControllerSuite.java
+++ src/test/java/com/matchandtrade/rest/v1/controller/ControllerSuite.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+	AuthenticationControllerIT.class,
 	UserControllerIT.class,
 	TradeControllerIT.class,
 	MembershipControllerIT.class,
